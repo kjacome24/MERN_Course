d2ad16b55c427c62d1ae7dcc7a570d55d83ae22b
kjacome24 <kevin.arturo.jacome.duque@gmail.com>
2024-02-15 18:47:14 -0500

    New Changes 15-02-2024 (Kevin)

--- MERN_Stack/test.js
+++ MERN_Stack/test.js
@@ -47,3 +47,134 @@ function update(param1, param2) {
 update(getter(), 6)
 console.log(state) //Output: [1, 2, 3, 4, 5, 6]
 
+
+
+// ------------------------
+var x = function (name) {
+    console.log('Hello World '+ name)
+}
+
+x("Kevin")
+
+// ------------------------
+const x = name => console.log('Hello World '+ name)
+x("Kevin")
+
+// ------------------------
+const x = (greeting,name) => console.log( greeting + name)
+x("hello ","Kevin")
+
+
+// ------------------------
+const x = (greeting,name) => {
+    console.log( greeting + name)
+    console.log( name)
+}
+
+
+x("hello ","Kevin")
+
+// ------------------------ Explicit return
+const x = (greeting,name) => {
+    return greeting + name
+}
+console.log(x("hello ","Kevin"))
+
+// ------------------------ Implicit return
+const x = (greeting,name) => greeting + name
+
+console.log(x("hello ","Kevin"))
+
+// ------------------------------------Examples Arrow Functions
+//1 ------------------------------------
+var sayHello = function(name) {
+    console.log('Hello ' + name);
+  };
+  
+sayHello('Kevin');  
+
+//2 ------------------------------------
+var sayHello = (name) => {
+    console.log(`Hello ${name}`);// ------------------------------------Complex string
+  };
+
+sayHello('Kevin');
+  
+//3 ------------------------------------
+var sayHello = name => console.log(`Hello ${name}`);
+sayHello('Kevin');
+
+// ------------------------ Explicit return for object literal
+const returnObjLonghand = () => {
+    return { 
+        firstName: 'John',
+        lastName: 'Wick'
+    }
+}
+// -------------------------- Implicit return for object literal
+const returnObj = () => ({ firstName: 'John', lastName: 'Wick' }) // Surround the object literal in parenthesis (()) to indicate an implicit return
+
+console.log(returnObj())
+
+
+
+
+// ------------------------------------Examples Arrow Functions
+const obj = {
+    name2: "John",
+    es5Method: function () {
+        console.log(obj.name2)
+    }
+};
+obj.es5Method(); 
+
+const {name2, es5Method} = obj;
+console.log(name2)
+
+es5Method()
+
+
+// ------------------------------------
+const obj = {
+    name: "John",
+    es5Method: function () {
+        console.log(obj.name)
+    }
+};
+const newObj = {...obj, name: "Joe"}
+//Logs the newObj object's properties
+obj.es5Method()
+console.log(obj)
+console.log(newObj)
+//Executes the inherited es5Method on the newObj
+newObj.es5Method()
+
+
+// ----------------------------------------Context this es5 
+
+const obj = {
+    name: "John",
+    es5Method: function () {
+        console.log(this.name)
+    }
+};
+const newObj = {...obj, name: "Joe"}
+//Logs the newObj object's properties
+console.log(newObj)
+//Executes the inherited es5Method on the newObj
+newObj.es5Method()
+
+
+// ----------------------------------------Context this es6
+
+const obj = {
+    name: "John",
+    arrowMethod: () => {
+       console.log(this.name)
+    }
+};
+const newObj = {...obj, name: "Joe"}
+//Logs the newObj object's properties
+console.log(newObj)
+//Executes the inherited arrowMethod on the newObj
+newObj.arrowMethod()
